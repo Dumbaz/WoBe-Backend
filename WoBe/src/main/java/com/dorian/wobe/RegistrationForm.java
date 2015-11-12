374bfaf68b2221c1acc90f1699f785db479e4f1a
Dorian <iwantmyfingerback+githubcommits@gmail.com>
2015-11-12 11:44:04 +0100

    Basic Register Template working
    Had to pass the Java Object -_-

--- WoBe/src/main/java/com/dorian/wobe/RegistrationForm.java
+++ WoBe/src/main/java/com/dorian/wobe/RegistrationForm.java
@@ -16,7 +16,15 @@ public class RegistrationForm {
 		return firstName;
 	}
 	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
 	public String getLastName() {
 		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 }
